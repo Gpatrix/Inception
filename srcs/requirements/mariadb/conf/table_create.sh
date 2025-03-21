3c0acfc90f904d376d780c758b6fd30bc84940ef
Gap <lchauvet@student.42lehavre.fr>
2025-03-21 12:21:00 +0100

    fix: better table_create for mariadb

--- srcs/requirements/mariadb/conf/table_create.sh
+++ srcs/requirements/mariadb/conf/table_create.sh
@@ -1,10 +1,11 @@
 rc-service mariadb start
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`";
-
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root -p << EOF
+CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`"
+CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
 
 rc-service mariadb stop;
