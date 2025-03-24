8141bb932592bceea3b5461aa9687d0221e40617
Gap <lchauvet@student.42lehavre.fr>
2025-03-24 13:17:00 +0100

    WIP: wordpess

--- srcs/requirements/mariadb/tool/table_create.sh
+++ srcs/requirements/mariadb/tool/table_create.sh
@@ -3,18 +3,16 @@
 set -e
 
 # rc-service mariadb start
-# /usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
+/usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
 
 until mariadb-admin ping --silent; do
 	echo "Waiting for MariaDB to be ready..."
 	sleep 2
 done
 
-# Run the SQL script
 if [ -f "/tools/init.sql" ]; then
 	envsubst < default.sql | mariadb
 fi
 
-echo "MariaDB is ready"
-sleep infinity
 # rc-service mariadb restart
+sleep infinity
--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -2,19 +2,23 @@
 
 until mysql -h mariadb -u ${SQL_USER} -p${SQL_PASSWORD} -e "SELECT 1" > /dev/null 2>&1; do
   echo "Waiting for MariaDB to be ready..."
-  sleep 5
+  sleep 3
 done
+
+sleep infinity
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
     echo "Creating WordPress configuration file..."
+    # wp core download --path='/var/www/wordpress/'
+    # wp core install --path='/var/www/wordpress/'
     wp config create --allow-root \
         --dbname="${SQL_DATABASE}" \
         --dbuser="${SQL_USER}" \
         --dbpass="${SQL_PASSWORD}" \
         --dbhost=mariadb:3306 \
-        --path='/var/www/wordpress'
+        --path='/var/www/wordpress/'
 else
     echo "WordPress configuration file already exists"
 fi
 
-exec /usr/sbin/php-fpm83", "-F"
+exec /usr/sbin/php-fpm83 -F
