d7cf1054990be96ea67bf4e9b0b8f54755358f19
Gap <lchauvet@student.42lehavre.fr>
2025-03-24 11:43:33 +0100

    WIP: mariadb

--- srcs/requirements/mariadb/tool/table_create.sh
+++ srcs/requirements/mariadb/tool/table_create.sh
@@ -3,16 +3,18 @@
 set -e
 
 # rc-service mariadb start
-/usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
+# /usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
 
-echo "Waiting for MariaDB to be ready..."
 until mariadb-admin ping --silent; do
+	echo "Waiting for MariaDB to be ready..."
 	sleep 2
 done
 
 # Run the SQL script
 if [ -f "/tools/init.sql" ]; then
-	envsubst < /tools/init.sql | mariadb
+	envsubst < default.sql | mariadb
 fi
 
+echo "MariaDB is ready"
+sleep infinity
 # rc-service mariadb restart
