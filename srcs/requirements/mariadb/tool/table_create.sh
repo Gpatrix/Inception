a7b3d787141af58036d9251456577473d7f3bd37
Gap <lchauvet@student.42lehavre.fr>
2025-03-25 13:29:02 +0100

    add: secutity for mariadb setup

--- srcs/requirements/mariadb/tool/table_create.sh
+++ srcs/requirements/mariadb/tool/table_create.sh
@@ -4,15 +4,23 @@ set -e
 
 /usr/bin/mariadbd-safe --datadir=/var/lib/mysql &
 
+mariadb_pid=$!
 
 until mariadb-admin ping --silent; do
 	echo "Waiting for MariaDB to be ready..."
 	sleep 2
 done
 
-if [ -f "default.sql" ]; then
-	envsubst < default.sql | mariadb
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
+	echo "Initializing database..."
+	if [ -f "default.sql" ]; then
+		envsubst < default.sql | mariadb
+	fi
+else
+	echo "Database already initialized."
 fi
 
-# TODO to change
-wait
+echo "SHUTDOWN;" | mariadb
+sleep 2
+
+exec /usr/bin/mariadbd-safe --datadir=/var/lib/mysql
